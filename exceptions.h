a506e1c996fefd1d1f0cf4463f6fa8afb56c8dfc
UnsignedLemon <101453812+UnsignedLemon@users.noreply.github.com>
2022-06-28 12:01:08 +0800

    Update exceptions.h

--- exceptions.h
+++ exceptions.h
@@ -8,6 +8,9 @@ class exception{};
 class open_file_failed : public exception{};
 class invalid_assembly_code : public exception{};
 class invalid_data_fetch : public exception{};
+class queue_empty : public exception{};
+class unknown_not_match : public exception{};
+class forwarding_not_match : public exception{};
 
 }
 
