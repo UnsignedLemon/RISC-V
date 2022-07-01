4dd7de841cecdef5b8954c319ed3ae4260c6d8e8
UnsignedLemon <101453812+UnsignedLemon@users.noreply.github.com>
2022-07-01 15:01:33 +0800

    Update resources.h

--- resources.h
+++ resources.h
@@ -5,32 +5,39 @@
 #include "data/data.h"
 #include "reg/reg.h"
 #include "units/units.h"
+#include "predictor/predictor.h"
 #include "enum.h"
 
 namespace riscv{
 	
+//--------------------------    DATA    ----------------------------------------
 	extern riscv::DATA memoryData;
 	
+//-------------------------    REGISTERS    ------------------------------------
 	extern riscv::REGISTER R[32];
 	extern riscv::BUFFER B1,B2,B3,B4;
 	extern riscv::_PC        PC;
 	
+//---------------------------    UNITS    --------------------------------------
 	extern riscv::_IF        IF;
 	extern riscv::_ID        ID;
 	extern riscv::_EX      	 EX;
 	extern riscv::_ME		 ME;
 	extern riscv::_WB		 WB;
 	
-	extern bool shouldTerminate;
-	extern const unsigned int terminateCmd;
+//-------------------------    PREDICTOR    ------------------------------------
+	extern riscv::PREDICTOR  PRED;
 	
+//---------------    Const Instruction Referred to as END    -------------------
+	extern const unsigned int terminateCmd;
+
+//-------------------------    ENVIRONMENT    ----------------------------------
+	extern bool shouldTerminate;
 	extern bool shouldPause;
 	extern bool shouldBubble;
 	extern bool shouldDiscard;
 	extern bool shouldPopPCQ;
-
+	extern bool isTaken;		// Used to update PREDICTOR.
 }
-
-extern std::string map[];
 
 #endif
